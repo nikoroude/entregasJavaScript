03ecf136f2b68833d30a692f13ee3f6496f343dd
nikoroude <nikoroude@gmail.com>
2022-05-29 19:35:09 -0300

    inicializando repo, cargando arhivos

---
+++ 1-CICLOS/main.js
@@ -0,0 +1,33 @@
+// TODO: SERIE FIBONACCI UTILIZANDO CICLO FOR
+
+/* let a=0 , b=1, c, i;
+alert("¡Bienvenido! Hagamos la serie de Fibonacci");
+alert("Por favor ingrese la cantidad de terminos que quiere generar");
+
+let cantidad = +(prompt("Ingrese un numero"));
+console.log("Cantidad de terminos: " + cantidad);
+console.log("0, ")
+
+for (i=3; i<=cantidad; i++){
+    c=a+b;
+    console.log(c + ", ");
+    a=b;
+    b=c;
+} */
+
+// TODO: El factorial de un número entero positivo se define como el producto de todos los números naturales anteriores o iguales a él.
+
+/*let a=1, f=1;
+
+alert("¡Bienvenido! Calculemos el factorial de un numero");
+let n = +(prompt("Por favor ingrese un numero entero positivo"));
+
+console.log("Factorial de " + n);
+
+while(n>1){
+    f= f * n;
+    n--;
+}
+
+console.log("El resultado es: " + f);
+alert(f);*/
