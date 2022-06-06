a929fd9a087109a8b6dbb7683f3e8bddb9e03dd9
nikoroude <nikoroude@gmail.com>
2022-06-06 20:22:45 -0300

    simulador

--- 2-SIMULADOR-INTERATIVO/app.js
+++ 2-SIMULADOR-INTERATIVO/app.js
@@ -1,11 +1,11 @@
 // ! Declaracion de variables
 
-let idTramite
+let idTramite = 0;
 let idPaciente
-let namePatient
 let entry
 let nombre
 let surname
+let namePatient
 let dataFunction
 let opcion
 
@@ -18,7 +18,6 @@ function requestEntry() {
     2. Nuevo trámite \n
     3. Esc`)
     console.log(opcion);
-    // return opcion
 }
 
 // opcion = prompt(`Por favor, ingrese el numero de la accion que desea realizar: \n 
@@ -31,23 +30,27 @@ function requestEntry() {
 
 function api() {
 
-    let opcion = requestEntry();
-    opcion = opcion.toLowerCase();
+    requestEntry();
 
-    while (opcion != "esc" || opcion != "3") {
+    while (opcion != "3") {
         if (opcion == "1") {
             let nombre = prompt("Por favor, ingrese el nombre del paciente: ")
             let surname = prompt("Por favor, ingrese el apellido del paciente: ")
             let idPaciente = prompt("Por favor, ingrese el numero de identificacion del paciente: ")
-            alert(`Nombre: ${nombre} \n Apellido: ${surname} \n Identificacion: ${idPaciente}`)
-            console.log(`Nombre: ${nombre} \n Apellido: ${surname} \n Identificacion: ${idPaciente}`)
+            namePatient = `${surname}, ${nombre}.`
+            namePatient = namePatient.toUpperCase();
+            alert(`Paciente ${namePatient} \nIdentificacion: ${idPaciente}`)
+            console.log(`Paciente ${namePatient} \nIdentificacion: ${idPaciente}`)
             requestEntry();
         } else if (opcion == "2") {
-            let idTramite = prompt("Por favor, ingrese el numero de identificacion del trámite: ")
+            idTramite = idTramite + 1;
             let idPaciente = prompt("Por favor, ingrese el numero de identificacion del paciente: ")
-            let namePatient = prompt("Por favor, ingrese el nombre del paciente: ")
+            let nombre = prompt("Por favor, ingrese el nombre del paciente: ")
+            let surname = prompt("Por favor, ingrese el apellido del paciente: ")
+            namePatient = `${surname}, ${nombre}.`
+            namePatient = namePatient.toUpperCase();
             alert(`Identificacion del trámite: ${idTramite} \n Identificacion del paciente: ${idPaciente} \n Nombre del paciente: ${namePatient}`)
-            console.log(`Identificacion del trámite: ${idTramite} \n Identificacion del paciente: ${idPaciente} \n Nombre del paciente: ${namePatient}`)
+            console.log(`Identificacion del trámite: ${idTramite} \n Identificacion del paciente: ${idPaciente} \n Paciente: ${namePatient}`)
             requestEntry();
         } else {
             alert("Usted ha salido del programa");
--- 2-SIMULADOR-INTERATIVO/main.js
+++ 2-SIMULADOR-INTERATIVO/main.js
@@ -1,12 +1,15 @@
 // ! Declaracion de variables
 
-let idTramite = 0;
-let idPaciente = 0;
+var idTramite = 0;
+var idPaciente = 0;
+let pedido;
+let obraSocial;
 let dni;
-let namePatient = `${apellido}, ${nombre}.`;
 let entry;
 let nombre;
 let apellido;
+let opcion = 0;
+let namePatient;
 let usuario;
 
 
@@ -25,42 +28,123 @@ let usuario;
 
 // get(namePatient);
 
-function requestEntry(){
-    option = prompt (`Bienvenido ${usuario}!! \n
+function requestEntry() {
+    opcion = prompt(`Bienvenido ${usuario}!! \n
     Por favor, ingrese ingrese la opcion que desea realizar \n
     1. Nuevo paciente \n
     2. Autorizaciones \n
     3. Salir`);
 }
 
-function incremento(atributo){
-    return atributo + 1;
+function choisePedido() {
+    pedido = +prompt(`¿Que desea autorizar? (Ingrese el numero segun corresponda) \n 1. Cirugia \n 2. RMN/TAC \n 3. Ecodoppler \n 4. Hemodinamia - Cateterismo \n 5. Gastroenterologia \n 6. Electroencefalograma / Electromiografia \n 7. Radiografias / Electrocardiograma / Laboratorios / Consulta`);
 }
 
-function autogestion(){
-    let opcion = requestEntry();
-    opcion = opcion.toLowerCase();
+function logIn(){
+    usuario = prompt("Por favor, ingresa tu nombre");
+    usuario = usuario.toUpperCase();
+}
 
-    while (opcion != "salir" || opcion != "3" || opcion != "esc"){
-        if (opcion=="1" || opcion=="nuevo" || opcion=="nuevo paciente"){
+function incremento(a) {
+    a + 1;
+    return a;
+}
+
+function autogestion() {
+    // logIn();
+    requestEntry();
+    // opcion = opcion.toLowerCase();
+
+    while (opcion != "3") {
+        if (opcion == "1") {
             alert(`Por favor, ingrese los datos del nuevo paciente que desea cargar.`);
-            let nombre = prompt("Nombre del paciente:");
-            let apellido = prompt("Apellido del paciente:");
-            let dni = prompt("DNI / LC / LE:");
-            incremento (idPaciente);
-            alert(`El nuevo paciente a sido cargado con el numero ${idpaciente} \n Paciente: ${namePatient} \n D.N.I: ${dni}`);
-            console.log(`El nuevo paciente a sido cargado con el numero ${idpaciente} \n Paciente: ${namePatient} \n D.N.I: ${dni}`);
+            nombre = prompt("Nombre del paciente:");
+            apellido = prompt("Apellido del paciente:");
+            namePatient = `${apellido}, ${nombre}.`
+            dni = prompt("DNI / LC / LE:");
+            incremento(idPaciente);
+            console.log(`El nuevo paciente a sido cargado con el numero ${idPaciente} \n Paciente: ${namePatient} \n D.N.I: ${dni}`)
+            alert(`El nuevo paciente ha sido cargado con el numero ${idPaciente} \n Paciente: ${namePatient} \n D.N.I: ${dni}`);
             requestEntry();
-        } else if (opcion=="2" || opcion=="autorizacion" || opcion=="autorizaciones"){
-            alert(`Por favor, ingrese los datos solicitados.`);
-            let nombre = prompt("Nombre del paciente:");
-            let apellido = prompt("Apellido del paciente:");
-            let dni = prompt("DNI / LC / LE:");
-            
-            incremento (idTramite);
-            alert(`El nuevo paciente a sido cargado con el numero ${idpaciente} \n Paciente: ${namePatient} \n D.N.I: ${dni}`);
-            console.log(`El nuevo paciente a sido cargado con el numero ${idpaciente} \n Paciente: ${namePatient} \n D.N.I: ${dni}`);
+        } else if (opcion == "2") {
+            let autorizacionOpcion = prompt(`Elija la opcion segun corresponda \n 1. PAMI \n 2. IOSPER \n 3. Otra obra social`);
+            autorizacionOpcion = autorizacionOpcion.toLocaleLowerCase();
+            if (autorizacionOpcion == "1" || autorizacionOpcion == "pami") {
+                alert(`Por favor, ingrese los datos solicitados.`);
+                nombre = prompt("Nombre del paciente:");
+                apellido = prompt("Apellido del paciente:");
+                dni = prompt("DNI / LC / LE:");
+                obraSocial = "PAMI";
+                choisePedido;
+                switch (pedido) {
+                    case 1:
+                        pedido = "CIRUGIA";
+                        break;
+                    case 2:
+                        pedido = "RMN / TAC";
+                        break;
+                    case 3:
+                        pedido = "ECODOPPLER";
+                        break;
+                    case 4:
+                        pedido = "HEMODINAMIA";
+                        break;
+                    case 5:
+                        pedido = "GASTROENTEROLOGIA";
+                        break;
+                    case 6:
+                        pedido = "EEG / EMG";
+                        break;
+                    case 7:
+                        pedido = "RX / ECG / LABORATORIO / CONSULTA";
+                        break;
+                    case 0:
+                        break;
+                    case pedido < 0 || pedido > 7:
+                        alert(`Dato ingresado no valido, por favor, intentelo nuevamente`);
+                        choisePedido();
+                        break;
+                }
+                incremento(idTramite);
+                alert(`El nuevo pedido ha sido cargado con el numero ${idTramite} \n Paciente: ${namePatient} \n D.N.I: ${dni} \n Obra social; ${obraSocial} \n Pedido: ${pedido}`);
+                console.log(`El nuevo pedido ha sido cargado con el numero ${idTramite} \n Paciente: ${namePatient} \n D.N.I: ${dni} \n Obra social; ${obraSocial} \n Pedido: ${pedido}`);
+                requestEntry();
+            } else if (autorizacionOpcion == "2" || autorizacionOpcion == "iosper") {
+                alert(`Por favor, ingrese los datos solicitados.`);
+                nombre = prompt("Nombre del paciente:");
+                apellido = prompt("Apellido del paciente:");
+                dni = prompt("DNI / LC / LE:");
+                obraSocial = "IOSPER";
+                pedido = "CIRUGIA";
+                incremento(idTramite);
+                alert(`El nuevo pedido ha sido cargado con el numero ${idTramite} \n Paciente: ${namePatient} \n D.N.I: ${dni} \n Obra social; ${obraSocial} \n Pedido: ${pedido}`);
+                console.log(`El nuevo pedido ha sido cargado con el numero ${idTramite} \n Paciente: ${namePatient} \n D.N.I: ${dni} \n Obra social; ${obraSocial} \n Pedido: ${pedido}`);
+                requestEntry();
+            } else if (autorizacionOpcion == "3" || autorizacionOpcion == "otra"){
+                alert(`Por favor, ingrese los datos solicitados.`);
+                nombre = prompt("Nombre del paciente:");
+                apellido = prompt("Apellido del paciente:");
+                dni = prompt("DNI / LC / LE:");
+                obraSocial = prompt("Obra Social");
+                obraSocial = obraSocial.toUpperCase();
+                pedido = "CIRUGIA";
+                incremento(idTramite);
+                alert(`El nuevo pedido ha sido cargado con el numero ${idTramite} \n Paciente: ${namePatient} \n D.N.I: ${dni} \n Obra social; ${obraSocial} \n Pedido: ${pedido}`);
+                console.log(`El nuevo pedido ha sido cargado con el numero ${idTramite} \n Paciente: ${namePatient} \n D.N.I: ${dni} \n Obra social; ${obraSocial} \n Pedido: ${pedido}`);
+                requestEntry();
+            } else {
+                alert("Los datos ingresados no son validos. Por favor, ingrese nuevamente");
+                requestEntry();
+            }
+        } else if (opcion<"1" || opcion>"3"){
+            alert("Los datos ingresados no son validos. Por favor, ingrese nuevamente");
             requestEntry();
+        } else{
+            alert("Usted ha salido del programa");
+            console.log("Usted ha salido del programa");
+            break;
         }
     }
 }
+
+autogestion();
